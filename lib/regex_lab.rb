fb665a81dcacde3bf8ba7c8e2ea2bfff060a4f9d
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-21 13:04:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{3,4}/) ? true : false
+  phone.match(/\d{3,4}\D\d{3,4}\D\d{4,5}/) ? true : false
 end
